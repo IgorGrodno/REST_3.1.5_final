46a71f65d1fa742f2a2cdea55e7af826abb4a394
noobprofesor <noobprofesor@gmail.com>
2023-04-01 20:11:34 +0300

    done

---
+++ src/main/java/ru/kata/spring/boot_security/demo/Initializer.java
@@ -0,0 +1,35 @@
+package ru.kata.spring.boot_security.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
+
+@Component
+public class Initializer {
+    private final UserService userService;
+
+    @Autowired
+    public Initializer(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostConstruct
+    public void init() {
+        User user = userService.getUserByEmail("admin@mail.ru");
+        if (user == null) {
+            Role userRole = new Role("ROLE_USER");
+            Role adminRole = new Role("ROLE_ADMIN");
+            List<Role> adminRoles = new ArrayList<>();
+            adminRoles.add(userRole);
+            adminRoles.add(adminRole);
+            User admin = new User(adminRoles,"admin", "admin", (byte) 30, "admin@mail.ru", "admin@mail.ru", "admin");
+
+            userService.save(admin);
+        }
+    }
+}
